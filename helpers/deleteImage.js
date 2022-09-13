bc8977ced1c2c142b7a0dd892cc2a960937b7595
Daniel Mihai <sasdaniel@daniels-mbp.home>
2022-09-13 21:28:03 +0100

    Replaced bcrypt with bcryptjs

--- helpers/deleteImage.js
+++ helpers/deleteImage.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 module.exports = {
     deleteImage: (imageLink) => {
-        const link = imageLink.split(com)[1];
+        const link = imageLink.split('com')[1];
         console.log(link);
         fs.unlinkSync('.' + link);
     }
